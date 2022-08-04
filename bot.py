904d01d7887776e131e9acf22ca973527c2c4deb
peterbuoy <peterbuoy@gmail.com>
2022-08-04 10:18:36 -0700

    rework reminder calculation to handle extended downtime

--- bot.py
+++ bot.py
@@ -7,52 +7,20 @@ from discord.ext import tasks, commands
 from dotenv import load_dotenv
 load_dotenv()
 
+
 conn = sqlite3.connect("cac.db")
 cursor = conn.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS mention (mentioned_user_discord_id TEXT, mention_timestamp INTEGER, mention_message_id TEXT, remind_time_in_hours INTEGER)")
+cursor.execute("CREATE TABLE IF NOT EXISTS mention (mentioned_user_discord_id TEXT, mention_timestamp INTEGER, mention_message_id TEXT, next_reminder_time_unix INTEGER)")
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix='%', intents=intents)
 
-def calc_next_remind_time(hours: int):
-	# Reminder times are hardcoded up to 24 hours. After 24 hours, reminder times are every 24 hours.
-	# 1, 2, 4, 6, 12, 24, 48, 72, 96, etc
-	if hours == 0:
-		return 1
-	elif hours == 1:
-		return 2
-	elif hours == 2:
-		return 4
-	elif hours == 4:
-		return 6
-	elif hours == 6:
-		return 12
-	elif hours == 12:
-		return 24
-	elif hours >= 24:
-		return hours + 24
+TARGETED_USER_ID = 131965968980246529
+# 131965968980246529 peter
+# 212395768273829890 david
+TARGET_CHANNEL_ID = 974545078695653439
+# 974545078695653439 test test channel
+# 274417939866714113 general channel
 
-def calc_prev_remind_time(hours: int):
-	if hours == 1:
-		return 0
-	elif hours == 2:
-		return 1
-	elif hours == 4:
-		return 2
-	elif hours == 6:
-		return 4
-	elif hours == 12:
-		return 6
-	elif hours == 24:
-		return 12
-	elif hours > 24:
-		return hours - 24
-
-def define_custom_func():
-	conn.create_function('calc_next_remind_time', 1, calc_next_remind_time)
-define_custom_func()
-
-TARGETED_USER_ID = 212395768273829890
-TARGET_CHANNEL_ID = 274417939866714113
 
 @bot.command()
 async def reminders(ctx):
@@ -70,10 +38,12 @@ async def reminders(ctx):
 	else:
 		await ctx.send("The reply queue is empty for now, good job!")
 
+
 @bot.event
 async def on_ready():
-	print(f'{bot.user.name}: Bot Ready')
+	print(f'{bot.user.name}: Bot Ready at ${datetime.datetime.now()}')
 	print('------')
+
 
 @bot.event
 async def on_message(message: discord.Message):
@@ -81,7 +51,9 @@ async def on_message(message: discord.Message):
 		return
 	if len(message.mentions) > 0 and TARGETED_USER_ID in map(lambda x: x.id, message.mentions) and message.reference is None:
 		unix_timestamp = int(datetime.datetime.now(tz=pytz.UTC).timestamp())
-		cursor.execute("INSERT INTO mention VALUES(?, ?, ?, ?)", (TARGETED_USER_ID, unix_timestamp, message.id, 1))
+		print('unix_timestamp: ', unix_timestamp)
+		# We add 3600 (seconds) to the unix timestamp to set the next_reminder_time_unix column to one hour after a valid mention message
+		cursor.execute("INSERT INTO mention VALUES(?, ?, ?, ?)", (TARGETED_USER_ID, unix_timestamp, message.id, unix_timestamp + 3600))
 		conn.commit()
 	# pinning a message will count as replying to a message
 	if (message.author.id == TARGETED_USER_ID and message.reference is not None):
@@ -94,24 +66,53 @@ async def on_message(message: discord.Message):
 			await message.channel.send(f"You have replied to one message with the following response:\n >>> {message.clean_content}")
 	await bot.process_commands(message)
 
+
+def calc_next_remind_interval_from_initial_mention(hours_elapsed: int):
+	if hours_elapsed >= 24:
+		return (hours_elapsed // 24) * 24
+	elif hours_elapsed >= 12:
+		return 24
+	elif hours_elapsed >= 6:
+		return 12
+	elif hours_elapsed >= 4:
+		return 6
+	elif hours_elapsed >= 2:
+		return 4
+	elif hours_elapsed >= 1:
+		return 2
+	# the 1 hour reminder is already baked into the row upon insertion
+	else:
+		print("Something has gone terrible wrong in calculating the next reminder time function.")
+
+
 @tasks.loop(seconds=60)
 async def remind_mentioned_to_reply():
-	testing_channel = await bot.fetch_channel(TARGET_CHANNEL_ID)
-	reminders = cursor.execute("UPDATE mention SET remind_time_in_hours = calc_next_remind_time(remind_time_in_hours) WHERE (remind_time_in_hours * 60 * 60) < (((julianday('now') - 2440587.5) * 86400.0) - mention_timestamp) RETURNING *").fetchall()
-	conn.commit()
+	target_channel = await bot.fetch_channel(TARGET_CHANNEL_ID)
+	reminders = cursor.execute("SELECT * from mention WHERE next_reminder_time_unix < ((julianday('now') - 2440587.5) * 86400.0)").fetchall()
+
 	if len(reminders) < 1:
 		return
-	# mentioned_user_discord_id TEXT, mention_timestamp INTEGER, mention_message_id TEXT, remind_time_in_hours INTEGER
-	# this only mentions the first person in the list... which is only one person anyway LOL
+
 	reminder_message = f"<@{TARGETED_USER_ID}>\nBelow are the message(s) you have not replied to in a timely manner.\nPlease Discord **reply** to the linked message to remove these reminders.\n"
 	for reminder in reminders:
+		hours_elapsed = (int(datetime.datetime.now(tz=pytz.UTC).timestamp()) - reminder[1]) // 3600
+		new_next_reminder_interval = calc_next_remind_interval_from_initial_mention(hours_elapsed)
+		# initial timestamp plus the next interval (calculated from the hours elapsed) will give you the new reminder time
+		# this is so that if the bot goes down, it will give the proper remind time
+		new_next_reminder_time = int(reminder[1]) + (new_next_reminder_interval * 3600)
+		print("hours elapsed for message with id, ", reminder[2], ",is ", hours_elapsed)
+		print("the new_next_reminder_interval is ", new_next_reminder_interval)
+		print("the message id is", reminder[2])
+		print("the new next reminder time is", new_next_reminder_time)
 		try:
-			message_with_mention_jump_url = (await testing_channel.fetch_message(reminder[2])).jump_url
-			reminder_message += f"{calc_prev_remind_time(reminder[3])} hour(s) ago.\n{message_with_mention_jump_url}\n\n"
+			message_with_mention_jump_url = (await target_channel.fetch_message(reminder[2])).jump_url
+			reminder_message += f"{(reminder[3] - reminder[1]) // 3600} hour(s) ago.\n{message_with_mention_jump_url}\n\n"
+			cursor.execute("UPDATE mention SET next_reminder_time_unix = ? WHERE mention_message_id = ?", (new_next_reminder_time, reminder[2]))
+			conn.commit()
 		except discord.NotFound:
 			cursor.execute("DELETE FROM mention WHERE mention_message_id = ?", (reminder[2],))
 			conn.commit()
-	await testing_channel.send(reminder_message)
+	await target_channel.send(reminder_message)
 		
 
 # not fault-tolerant at all, must put into load and unload cog that starts and stops
